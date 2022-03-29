46181f078be86445ea68ffe2604c77030715d9c5
Eric Gonzalez <ejoglez@gmail.com>
2022-03-29 08:42:44 -0500

    find the errors

--- db/migrate/20210721161556_create_companies.rb
+++ db/migrate/20210721161556_create_companies.rb
@@ -1,6 +1,6 @@
 class CreateCompanies < ActiveRecord::Migration[6.1]
   def change
-    create_table :companies do |t|
+    create_table :companie do |t|
       t.string :name
       t.integer :founding_year
     end
